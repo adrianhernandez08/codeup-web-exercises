2175649cf45f51127b596b9fd6206169e381b3cd
Adrian Hernandez <adrian.c.hernandez08@gmail.com>
2023-04-15 00:15:16 -0500

    total price again

--- js/external.js
+++ js/external.js
@@ -1,7 +1,9 @@
-let littleMermaid = prompt("How many days did you rent Little Mermaid?");
-let brotherBear = prompt("How many days did you rent Brother Bear?");
-let hercules = prompt("How many days did you rent Hercules?");
-let pricePerDay = 3
-let totalPrice = (Number(litleMermaid) + Number(brotherBear) + Number(hercules));
+function totalPrice() {
+    let littleMermaid = prompt("How many days did you rent Little Mermaid?");
+    let brotherBear = prompt("How many days did you rent Brother Bear?");
+    let hercules = prompt("How many days did you rent Hercules?");
+    let pricePerDay = 3
+    let totalPrice = (Number(litleMermaid) + Number(brotherBear) + Number(hercules));
+}
 alert("Your total rental price is " + "$" + totalPrice());
 
