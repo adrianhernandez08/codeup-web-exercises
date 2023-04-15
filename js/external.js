ec258213c9130e0127b7213125e3e8f52a3334df
Adrian Hernandez <adrian.c.hernandez08@gmail.com>
2023-04-14 23:31:12 -0500

    alert

--- js/external.js
+++ js/external.js
@@ -1,3 +1,7 @@
+
 console.log("Hello from external JavaScript")
 alert("Welcome to my Website!")
+prompt("What is your favorite color?")
+favoriteColor = prompt("What is your favorite color?")
+alert("Great, my favorite color is" + favoriteColor + "also.")
 
