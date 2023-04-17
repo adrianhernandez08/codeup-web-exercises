d83f758cb6576c18a9ae35bd66554326438db525
Adrian Hernandez <adrian.c.hernandez08@gmail.com>
2023-04-17 08:51:55 -0500

    added questions 3.3

--- js/external.js
+++ js/external.js
@@ -21,6 +21,15 @@ function totalPay() {
 alert("Your total pay for the week is $ " + totalPay + ".")
 
 
-
+alert("Beginning enrollment");
+function schedule(){
+    var notConflicting =confirm("If class is full click 'cancel'. If class is not full, click 'ok'");
+    var scheduleConflict = confirm("Does this conflict with your schedule? Click 'ok' if it does not.");
+    if(nonConflicting === true && scheduleConflict === true){
+        alert("You can register for this class");
+    } else{
+        alert("Sorry, you will not be able to attend this class.");
+    }
+}
 
 
