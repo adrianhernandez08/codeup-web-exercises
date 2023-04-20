49a121fbece29a3704f0ce6e5ed10f99b1842912
Adrian Hernandez <adrian.c.hernandez08@gmail.com>
2023-04-20 14:32:06 -0500

    added loops

---
+++ js/break_and_continue.js
@@ -0,0 +1,49 @@
+
+
+ // let oddNumber = prompt("give me an odd number between 1 and 50");
+ //
+ //        if (oddNumber % 2 != 0) {
+ //            console.log(oddNumber + " is an odd number");
+ //        } else {
+ //           prompt("The number " + oddNumber + " is an even number. Please enter an odd number between 0 and 50");
+ //        }
+ //    console.log(oddNumber);
+ //
+ // for (var x = 1; x < 50; x += 2) {
+ //     if (x === oddNumber) {
+ //         console.log(`SKIPPED'  ${oddNumber}`)
+ //         continue
+ //     }
+ //}
+ // {
+ //
+ //     let oddNumber = true;
+ //
+ //     while (oddNumber === false) {
+ //         oddNumber = prompt("give me an odd number");
+ //     }
+ //
+ //     do {
+ //         oddNumber = prompt("Please, give me and odd number?");
+ //     } while (oddNumber === false)
+ //
+ // }
+
+//
+//  let oddNumber = parseFloat(prompt("give me an odd number between 1 and 50"));
+//  do {
+//      if (oddNumber % 2 === 0) {
+//          oddNumber = parseFloat(prompt("The number " + oddNumber + " is an even number. Please enter an odd number between 1 and 50"));
+//      }
+//  } while (oddNumber % 2 === 0)
+//
+// for (let even = 1; even < 50; even += 2){
+//     if (even === oddNumber){
+//         console.log(`We skipped! ${oddNumber}`)
+//         continue
+//     }
+//     console.log('odd number:' + even);
+// }
+//
+//
+//
---
+++ js/for_loops.js
@@ -0,0 +1,40 @@
+// 'use strict'
+
+// //2...multiplication table
+//     const number = parseInt(prompt("enter a number"));
+//     for (let i = 1; i <= 10; i++) ;
+//     {
+//         const result = i * number;
+//         console.log(`${number} * ${j} = ${result}`);
+//     }
+//3....even or odd (random numbers).........................
+// {
+//        var number = Math.floor((Math.random()* 200) + 1);
+//
+//         if (number % 2 == 0) {
+//             console.log(number + " is an even number");
+//         } else {
+//             console.log("an odd number is " + number);
+//         }
+//     }
+//4.........number christmas tree
+//
+// for (i = 1; i <= 9; i++) {
+//     var res = [];
+//     for (a = 1; a <= i; a++){
+//         res.push(i);
+//     }
+//     console.log(res.join(""));
+// }
+
+//5......... count down by 5.......
+// for (i = 100; i > 0; i -=5){
+//     console.log(i);
+// }
+
+// let sum = 5;
+// do {
+//     console.log(sum);
+//     sum += 5;
+// } while (sum < 100)
+
