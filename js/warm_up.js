e1d55ff3b76b1241b3d9d761abb3760a7db056c5
Adrian Hernandez <adrian.c.hernandez08@gmail.com>
2023-05-04 09:39:24 -0500

    04May warmup exercise

--- js/warm_up.js
+++ js/warm_up.js
@@ -35,20 +35,20 @@
 //02 MAY 2023
 // Write a function that takes in a string and counts the instances of the letter 'e' within the argument. The count should be case insensitive. If the string does not contain any 'e's it should return 0. If the argument is not a string it should return false.
 
-function countEs(str)  {
-    if (typeof str === "string"){
-        let count = 0;
-        let normalizedStr = str.toLowerCase();
-        for (let i = 0; i < str.length; i++){
-            if (normalizedStr.charAt(i) === "e"){
-                count++;
-        }
-    }
-    return count;
-}else{
-    return false;
-}
-}
+// function countEs(str)  {
+//     if (typeof str === "string"){
+//         let count = 0;
+//         let normalizedStr = str.toLowerCase();
+//         for (let i = 0; i < str.length; i++){
+//             if (normalizedStr.charAt(i) === "e"){
+//                 count++;
+//         }
+//     }
+//     return count;
+// }else{
+//     return false;
+// }
+// }
 
 // console.log(countEs("Ease")); // returns 2;
 // console.log(countEs("teleconference")); // returns 5;
@@ -68,18 +68,40 @@ function countEs(str)  {
 //         return false;
 //     }
 // }
-function containsE(str){
-    if (typeof str !== "string"){
-        return false
-    } else {
-        return str.toLowerCase().indexOf("e") > -1;
+// function containsE(str){
+//     if (typeof str !== "string"){
+//         return false
+//     } else {
+//         return str.toLowerCase().indexOf("e") > -1;
+//     }
+// }
+//
+//
+// console.log(containsE("Ease")); // returns 2;
+// console.log(containsE("teleconference")); // returns 5;
+// console.log(containsE("TOM")); // returns 0;
+// console.log(containsE(true)); // returns false;
+// console.log(containsE(['e', 'E'])); // returns false;
+// console.log(containsE()); // returns false;
+
+///////////////04 MAY 2023////////////////////////////////////
+// Dragon Warmup:
+//     Write a function named fizzBuzz that prints to the console the number 1 - 100. If the number is divisible by 3 print fizz instead of the number. If the number is divisible by 5 print buzz. If the number is divisible by both 3 and 5 print fizzBuzz.
+//
+//
+//
+function fizzBuzz()  {
+        for (let i = 1; i <= 100; i++) {
+            console.log(i);
+            if (i % 3 === 0 && i % 5 === 0) {
+                console.log("fizzBuzz");
+            } else if (i % 3 === 0) {
+                console.log("fizz");
+            } else if (i % 5 === 0) {
+                console.log("buzz");
+            } else {
+                console.log(i);
+            }
+        }
     }
-}
-
-
-console.log(containsE("Ease")); // returns 2;
-console.log(containsE("teleconference")); // returns 5;
-console.log(containsE("TOM")); // returns 0;
-console.log(containsE(true)); // returns false;
-console.log(containsE(['e', 'E'])); // returns false;
-console.log(containsE()); // returns false;
+fizzBuzz();
