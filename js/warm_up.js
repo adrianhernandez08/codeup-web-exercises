99e8edac3d198bdd27970f9d8695a167b8caaecc
Adrian Hernandez <adrian.c.hernandez08@gmail.com>
2023-05-18 15:22:53 -0500

    1522 commit

--- js/warm_up.js
+++ js/warm_up.js
@@ -235,17 +235,32 @@ const hamsters = [
 
 // Write a JavaScript function that takes in a number and returns an object with the following fields: number, which will contain the original number; evenOrOdd, which will contain a string ("even" or "odd") as to whether the value is even or odd; factors, an array of numbers that are evenly divisible within the number passed; and numberOfDigits, a number counting the number of digits with the number passed.
 
-function describeNumber(num) {
-    return {
-        number: num,
-        evenOrOdd: (num % 2 === 0) ? "even" : "odd",
-        factors: findFactors(num),
-        numberOfDigits: (`${num}`).length
+// function describeNumber(num) {
+//     return {
+//         number: num,
+//         evenOrOdd: (num % 2 === 0) ? "even" : "odd",
+//         factors: findFactors(num),
+//         numberOfDigits: (`${num}`).length
+//
+//     }
+//
+//
+//     console.log(3 % 10)
+//
+//     array.splice(array.indexOf('Ringo'), 0, 'Yoko');
+//
+//
+//
+// }
+//
+// console.log(describeNumber(19)); // returns {number: 19, evenOrOdd: "odd", factors: [1,19], numberOfDigits: 2};
+// console.log(describeNumber(2)); // returns {number: 2, evenOrOdd: "even", factors: [1,2], numberOfDigits: 1};
 
-    }
 
-
+band.shred = function() {
+    console.log('RAAAAAAAAAA!!!!');
 }
 
-console.log(describeNumber(19)); // returns {number: 19, evenOrOdd: "odd", factors: [1,19], numberOfDigits: 2};
-console.log(describeNumber(2)); // returns {number: 2, evenOrOdd: "even", factors: [1,2], numberOfDigits: 1};
+console.log(shred());;
+
+
