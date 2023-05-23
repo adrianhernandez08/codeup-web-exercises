e3a2519dd77700fdfec7a6fbe716f79dfc2e13b9
Adrian Hernandez <adrian.c.hernandez08@gmail.com>
2023-05-23 15:27:08 -0500

    konami function

--- js/jquery.js
+++ js/jquery.js
@@ -1,20 +1,15 @@
 "use strict"
 
 
-$(function() {
-    $('li, p, h1').css('background-color', '#FF0');
+$('#title').click(
+    function() {
+        $(this).css('background-color', '#FF0');
+    },
+);
 
-    // alert( 'The DOM has finished loading!' );
-});
-
-// $('*').css('border', '1px dashed #F00');
-
-
-$('#title');
-
-$('p')
-
-$('list-group')
-
-$('important')
+$('p').dblclick(
+    function() {
+        $(this).css('font-size', '18px');
+    },
+);
 
---
+++ js/konami.js
@@ -0,0 +1,53 @@
+"strict"
+
+// a key map of allowed keys
+var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    65: 'a',
+    66: 'b',
+    13: 'enter'
+};
+
+// the 'official' Konami Code sequence
+var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a', 'enter'];
+
+// a variable to remember the 'position' the user has reached so far.
+var konamiCodePosition = 0;
+
+// add keydown event listener
+document.addEventListener('keydown', function(e) {
+    // get the value of the key code from the key map
+    var key = allowedKeys[e.keyCode];
+    // get the value of the required key from the konami code
+    var requiredKey = konamiCode[konamiCodePosition];
+
+    // compare the key with the required key
+    if (key == requiredKey) {
+
+        // move to the next key in the konami code sequence
+        konamiCodePosition++;
+
+        // if the last key is reached, activate cheats
+        if (konamiCodePosition == konamiCode.length) {
+            activateCheats();
+            konamiCodePosition = 0;
+        }
+    } else {
+        konamiCodePosition = 0;
+    }
+});
+
+function activateCheats() {
+    document.body.style.backgroundImage = "url('img/background_to_party.gif')";
+    var audio = new Audio('img/party.mp3');
+    audio.play();
+    $('h1').on(
+        function() {
+            $(this).css('font-size', '100px');
+        },
+    );
+    alert("NOW THE PARTY CAN BEGIN!!!!");
+}
