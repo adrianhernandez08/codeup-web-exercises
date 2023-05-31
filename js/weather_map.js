67720b9bfca41efb7082767f11ca19d8e0251122
Adrian Hernandez <adrian.c.hernandez08@gmail.com>
2023-05-31 11:22:31 -0500

    initial weather map commit

---
+++ js/weather_map.js
@@ -0,0 +1,12 @@
+$('#title').click(
+    function() {
+        $(this).css('background-color', '#FF0');
+    },
+);
+
+$('p').dblclick(
+    function() {
+        $(this).css('font-size', '18px');
+    },
+);
+
