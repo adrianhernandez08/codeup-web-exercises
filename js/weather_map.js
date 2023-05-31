63bfb88fc3ca3006cd579d9f8c9345b04a788685
Adrian Hernandez <adrian.c.hernandez08@gmail.com>
2023-05-31 16:07:22 -0500

    1607 commit

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,12 +1,2 @@
-$('#title').click(
-    function() {
-        $(this).css('background-color', '#FF0');
-    },
-);
 
-$('p').dblclick(
-    function() {
-        $(this).css('font-size', '18px');
-    },
-);
 
